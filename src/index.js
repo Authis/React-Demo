4d021387a59f798745d638698c21fa464dfd0023
authis-sahadevan <authis.sahadevan@wipro.com>
2020-02-13 20:42:07 +0000

    adding circle ci and firebase

--- src/index.js
+++ src/index.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 class App extends Component {
+
+state = {
+   count : 1,
+}
+
+increment = () => {
+  let currval = this.state.count;
+    this.setState({
+      count :currval + 1,
+    });
+
+}
+decerement = () => {
+  let currval = this.state.count;
+  this.setState({
+    count :currval - 1,
+  });
+
+}
+
   render() {
-    return <h1>Hello, World</h1>;
+    return (
+     <div>
+      <h1>{this.state.count}</h1>
+      <div><button onClick={this.increment}>+</button></div>
+       <div><button onClick={this.decerement}>-</button></div>
+     </div>);
   }
 }
 
